c0345652393258e592587cd1ba29a3cca5a74ba8
Cheikhani <skibnewrim@gmail.com>
2020-08-09 12:03:03 -0700

    install and use validator npm package

--- note-app/app.js
+++ note-app/app.js
@@ -1,5 +1,8 @@
 //const fs = require('fs');
 //const name = require('./utils.js');
+
+const validator = require('validator');
 const getNotes = require('./notes.js');
 
+console.log(validator.isEmail("hsdyahoo.fr"));
 console.log('You are Welecom ... ',getNotes()); 
--- note-app/utils.js
+++
@@ -1,9 +0,0 @@
-console.log("\n import me ...");
-
-const name = 'Mohamed';
-
-const add = (a,b) => {
-    return a + b;
-}
-
-module.exports = add,name;
